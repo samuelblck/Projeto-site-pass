4d759e4641f783783ac4bd4b8baaa87629d12502
gnneto <130266936+gnneto@users.noreply.github.com>
2023-07-21 21:07:26 -0300

    Update Projeto-site.js

--- Projeto-site.js
+++ Projeto-site.js
@@ -1,6 +1,29 @@
 function entrar(){
-    var user = window.document.getElementById ("user1").value;
+    var loginTeste = 'email@email.com' // Simula um login qualquer para teste
+    var senhaTeste = 'senha123'        // Simula uma senha qualquer para teste
+
+    var user = window.document.getElementById("user1").value.toLowerCase();
     var pass = window.document.getElementById("pass1").value
     var ent = window.document.getElementById("entrada1")
-    ent.innerHTML = `Bem vindo ${user}`
+    // ent.innerHTML = `Bem vindo ${user}`
+    
+    if (user === loginTeste && pass === senhaTeste){ // Verifica se o email e senha informado bate com o da variavel
+        window.location.href = `paginaInicial.html?user=${encodeURIComponent(user)}`; // Caso o login for verdadeiro, ele vai pra pagina inicial passando o email na URL
+
+    }else if (pass != senhaTeste || user != loginTeste ){ // Usuario ou senha incorreto apenas avisa.
+        ent.style.color = 'red'
+        ent.innerHTML = ''
+        ent.innerHTML = `Usuario ou senha incorretos.` 
+
     }
+}
+
+function paginaInicial(user){
+    var msgBoasVindas = document.getElementById('msgBoasVindas')
+    msgBoasVindas.style.color = 'black'
+    msgBoasVindas.innerHTML = `Usuario conectado: ${user}`
+}
+
+function cadastro(){
+    window.location.href = 'cadastro.html' // Difeciona o usuario para a pagina cadastro
+}
