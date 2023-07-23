14086a02fe31639acd0047d4165f3a1fb796ef9e
samuelblck <46336583+samuelblck@users.noreply.github.com>
2023-07-23 14:11:09 +0000

    git push main

--- Projeto-site.js
+++ Projeto-site.js
@@ -27,3 +27,17 @@ function paginaInicial(user){
 function cadastro(){
     window.location.href = 'cadastro.html' // Difeciona o usuario para a pagina cadastro
 }
+
+//função para ocultar e mostrar a senha//
+function mostrarsenha(){
+    var inputpass = document.getElementById('pass1')
+    var btnmostrarsenha = document.getElementById('ocultar')
+
+    if(inputpass.type === 'password'){
+        inputpass.setAttribute('type','text')
+        btnmostrarsenha.classList.replace('bi-eye', 'bi-eye-slash')
+    }else  {
+        inputpass.setAttribute('type','password')
+        btnmostrarsenha.classList.replace('bi-eye-slash', 'bi-eye')   
+    }
+}
