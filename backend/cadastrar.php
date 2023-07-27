292852db8f302588ad241a19230d184130c238a3
gnneto <guilhermeacacio3@gmail.com>
2023-07-27 19:54:17 -0300

    Att login/cadastro

--- backend/cadastrar.php
+++ backend/cadastrar.php
@@ -1,27 +1,72 @@
 <?php
 include "conexao.php"; // Puxa o arquivo conexao
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") { // Só entra aq se o metodo do formulario for post
+$msg = ""; // Armazena a mensagem
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") { // Só entra aqui se o método do formulário for POST
     // Puxa email e senha do HTML
     $email = strtolower($_POST["email"]);
     $senha = $_POST["senha"];
 
-    $senhaHash = password_hash($senha, PASSWORD_BCRYPT); // Joga hash na senha e salva no banco ja com hash
-    
-
-
-    $query = "INSERT INTO usuarios (senha_hash, email) VALUES (:senha, :email)"; // Joga no banco os valores senha e email
-    $stmt = $banco->prepare($query); // Prepara a instrucao
-    // vincula o email/senha para o parametro  :senha e :email no sql
-    $stmt->bindParam(':senha', $senhaHash);
-    $stmt->bindParam(':email', $email);
-
-    // Se deu tudo certo ok se nao moio prkl
-    if ($stmt->execute()) {
-        echo "Cadastro realizado com sucesso!";
+    if (strlen($senha) < 8) {
+        $msg = "A senha deve ter no minimo 8 caracteres.";
     } else {
-        echo "Erro ao cadastrar.";
+        // Verifica se o email ja existe no banco de dados
+        $queryValidarEmail = "SELECT COUNT(*) AS total FROM usuarios WHERE email = :email";
+        $stmtValidarEmail = $banco->prepare($queryValidarEmail);
+        $stmtValidarEmail->bindParam(':email', $email);
+        $stmtValidarEmail->execute();
+
+        $resultado = $stmtValidarEmail->fetch();
+        $total_usuarios_com_email = $resultado['total'];
+
+        if ($total_usuarios_com_email > 0) {
+            // Email em uso
+            $msg = "O email ja esta sendo usado";
+        } else {
+            $senhaHash = password_hash($senha, PASSWORD_BCRYPT);
+
+            $query = "INSERT INTO usuarios (senha_hash, email) VALUES (:senha, :email)";
+            $stmt = $banco->prepare($query);
+            $stmt->bindParam(':senha', $senhaHash);
+            $stmt->bindParam(':email', $email);
+
+            if ($stmt->execute()) {
+                $msg = "Cadastro realizado";
+                // Limpa o formulario
+                // $_POST["email"] = "";
+                // $_POST["senha"] = "";
+            } else {
+                $msg = "Erro ao cadastrar.";
+            }
+            
+        }
     }
 }
-
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+</head>
+<body>
+    <h1>Cadastro</h1>
+    <section>
+        <div>
+            <form action="cadastrar.php" method="post">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+                <br>
+                <label for="senha">Senha:</label>
+                <input type="password" id="senha" name="senha" required>
+                <br>
+                <input type="submit" value="Cadastrar">
+            </form>
+            <?php if ($msg !== "") { ?>
+                <p><?php echo $msg; ?></p>
+            <?php } ?>
+            <a href="../index.html">Fazer login</a>
+        </div>
+    </section>
+</body>
+</html>
